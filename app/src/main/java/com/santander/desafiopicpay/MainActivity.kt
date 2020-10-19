c077d2d726c25f6546657e8632a94e9d1d019816
VanSharine <vanessasharine@gmail.com>
2020-10-19 00:01:28 -0300

    TChamada da tela do cartão de crédito finalizada

--- app/src/main/java/com/santander/desafiopicpay/MainActivity.kt
+++ app/src/main/java/com/santander/desafiopicpay/MainActivity.kt
@@ -3,7 +3,9 @@ package com.santander.desafiopicpay
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
+import com.santander.desafiopicpay.RegisterCreditCardActivity.Companion.KEY_CREDIT_CARD_NUMBER
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //val numberCreditCard = intent.getStringExtra(KEY_CREDIT_CARD_NUMBER)
+       // Log.i('número do Cartao',numberCreditCard?:"")
+
+       // etCreditCardNumber.text = Editable.Factory.getInstance().newEditable(numberCreditCard)
+        //println(numberCreditCard)
        // Log.i('df',"onCreate")
-        initComponents()
+      //  initComponents()
     }
     private fun initComponents(){
         etCreditCardNumber.addTextChangedListener(MaskWatcher(etCreditCardNumber,getString(R.string.credit_card_mask)))
         etCreditCardExpiration.addTextChangedListener(MaskWatcher(etCreditCardExpiration, getString(R.string.expiration_mask)))
         etCreditCardCvv.addTextChangedListener(MaskWatcher(etCreditCardCvv, getString(R.string.cvv_mask)))
+
 
         /*btCreditCardSave.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
--- app/src/main/java/com/santander/desafiopicpay/RegisterCreditCardActivity.kt
+++ app/src/main/java/com/santander/desafiopicpay/RegisterCreditCardActivity.kt
@@ -1,11 +1,27 @@
 package com.santander.desafiopicpay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register_credit_card.*
 
 class RegisterCreditCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_credit_card)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        btRegisterCreditCard.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            intent.putExtra(KEY_CREDIT_CARD_NUMBER,1111222233334444)
+            startActivity(intent)
+        }
+    }
+
+    companion object{
+        const val KEY_CREDIT_CARD_NUMBER = "number_credit_card"
     }
 }
